9df2e783caeb2f0f5cfd7461a191534333d53653
ragman <email@ragman.net>
2026-03-11 15:17:35 -0400

    text now scrolls as you type. Currently holds 10 pages of 168 characters. Seeing some delay when I type, I think due to SD card write latency

--- Digital_Notebook/Digital_Notebook.ino
+++ Digital_Notebook/Digital_Notebook.ino
@@ -6,127 +6,142 @@
 #include <Adafruit_SSD1306.h>
 #include <string>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define SD_SCK   D8
-#define SD_MISO  D9
-#define SD_MOSI  D10
-#define SD_CS    D2
+#define SD_SCK D8
+#define SD_MISO D9
+#define SD_MOSI D10
+#define SD_CS D2
 
 SPIClass sdSPI(FSPI);
+
+// TODO: need to think about supporting non-ascii characters
+
+// The default graphics font is a 6px by 8px font.
+// Our 128px screen can fit 21 chars across (21 * 6px = 126px out of 128),
+// and can display 8 lines (8 * 8px = 64px out of 64).
+const int PAGE_SIZE = 10;
+const int COL_NUM = 21;
+const int ROW_NUM = 8 * PAGE_SIZE;
+
+char buffer[ROW_NUM][COL_NUM];
+
+uint8_t cursorRow = 0;
+uint8_t cursorCol = 0;
 
 void printToScreen(const char *s) {
   Serial.printf("Displaying: %s\n", s);
   display.clearDisplay();
-  display.setTextSize(1); // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0, 0); // Start at top-left corner
+  display.setTextSize(1);               // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);  // Draw white text
+  display.setCursor(0, 0);              // Start at top-left corner
   display.print(s);
   display.display();
 }
 
-// TODO: need to think about supporting non-ascii characters
-// TODO: could replace buffer with manually calculating where to draw incoming characters
+void printBuffer() {
+  display.clearDisplay();
+  display.setTextSize(1);               // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);  // Draw white text
+  display.setCursor(0, 0);              // Start at top-left corner
 
-// 168 comes from the font being a 6x8 font.
-// Our 128px screen can fit 21 chars across (21 * 6px = 126px out of 128), 
-// and can display 8 lines (8 * 8px = 64px out of 64).
-const int BUFFER_SIZE = 168;
-char buffer[BUFFER_SIZE];
-uint16_t bufferIndex = 0;
+  int startCol = 0;
 
-void addToBuffer(uint8_t ascii) {
-  // wrap back around to the beginning.
-  if (bufferIndex == BUFFER_SIZE) {
-    buffer[0] = '\0';
-    bufferIndex = 0;
-    display.clearDisplay();
+  int startRow;
+  if (cursorRow <= 7) {
+    startRow = 0;
+  } else {
+    startRow = cursorRow - 7;
   }
 
-  buffer[bufferIndex] = ascii;
-  bufferIndex++;
+  for (int row = startRow; (row < startRow + 8) && (row < ROW_NUM); row++) {
+    for (int col = 0; col < COL_NUM; col++) {
+      if (buffer[row][col] == '\0') {
+        continue;
+      }
+
+      if (buffer[row][col] == 13) {
+        display.println();
+      } else {
+        display.print(buffer[row][col]);
+      }
+    }
+  }
+
+  display.display();
 }
 
-void deleteLastFromBuffer() {
-  if (bufferIndex <= 0) {
-    return;
+void updateFile() {
+  File file = SD.open("/diary.txt", FILE_WRITE);
+
+  for (uint8_t row = 0; row < ROW_NUM; row++) {
+    for (uint8_t col = 0; col < COL_NUM; col++) {
+      if (buffer[row][col] == '\0') {
+        continue;
+      }
+
+      if (buffer[row][col] == 13) {
+        file.println();
+      } else {
+        file.print((char)(buffer[row][col]));
+      }
+    }
   }
 
-  bufferIndex--;
-  buffer[bufferIndex] = '\0';
+  file.close();
+}
+
+void handleKeypress(uint8_t ascii) {
+  if (' ' <= ascii && ascii <= '~') {
+    buffer[cursorRow][cursorCol] = ascii;
+    cursorCol++;
+  } else if (ascii == 13 /* newline */) {
+    buffer[cursorRow][cursorCol] = ascii;
+    cursorCol = 0;
+    cursorRow++;
+  } else if (ascii == 8 /* backspace */) {
+    if (cursorRow == 0 && cursorCol == 0) {
+      return;
+    }
+
+    if (cursorCol == 0) {
+      cursorRow--;
+      cursorCol = COL_NUM - 1;
+      while (cursorCol > 0) {
+        if (buffer[cursorRow][cursorCol] != '\0') {
+          break;
+        }
+        cursorCol--;
+      }
+    } else {
+      cursorCol--;
+    }
+
+    buffer[cursorRow][cursorCol] = '\0';
+  }
+
+  // TODO: ideally would have a pageup/pagedown handler as well
+
+  if (cursorCol >= COL_NUM) {
+    cursorCol = 0;
+    // TODO: need to handle reaching the end of cursorRow better. Clear out the buffer and go to the top?
+    if (cursorRow < ROW_NUM) {
+      cursorRow++;
+    }
+  }
+
+  printBuffer();
+  updateFile();
 }
 
 class MyEspUsbHost : public EspUsbHost {
   void onKeyboardKey(uint8_t ascii, uint8_t keycode, uint8_t modifier) {
-    if (' ' <= ascii && ascii <= '~') {
-      File file = SD.open("/diary.txt", FILE_APPEND);
-      if (!file) {
-        printToScreen("error: SD Card write failed");
-        return;
-      }
-
-      file.print((char)ascii);
-      file.close();
-
-      digitalWrite(21, LOW);
-      delay(10);
-      digitalWrite(21, HIGH);
-
-      addToBuffer(ascii);
-      printToScreen(buffer);
-      return;
-    }
-
-    if (ascii == '\r' || ascii == '\n') {
-      File file = SD.open("/diary.txt", FILE_APPEND);
-      if (!file) {
-        printToScreen("error: SD Card write failed");
-        return;
-      }
-
-      // TODO: this may cause double newlines when \r\n is printed (aka on windows)
-      file.println();
-      file.close();
-
-      // TODO: need to clear screen if newlines are going to have the buffer extend past bottom of screen
-      addToBuffer('\n');
-      printToScreen(buffer);
-      return;
-    }
-    
-    if (ascii == 8 /* backspace */) {
-      File file = SD.open("/diary.txt", FILE_READ);
-      if (!file) {
-        printToScreen("error opening in rw");
-        return;
-      }
-
-      String content = file.readString();
-      file.close();
-
-      if (content.length() <= 0) {
-        return;
-      }
-
-      // removes the last char from content
-      // TODO: need to remove both /n and /r if they're there
-      content.remove(content.length() - 1);
-
-      // opening the file for write deletes the existing content
-      file = SD.open("/diary.txt", FILE_WRITE);
-
-      file.print(content);
-      file.close();
-
-      deleteLastFromBuffer();
-      printToScreen(buffer);
-      return;
-    }
+    handleKeypress(ascii);
   };
 };
 
